7fb546a7a79800c8fc1d2db2f45440b5f6b0d318
NeoBlox10 <neoblox10@gmail.com>
2022-03-22 23:14:54 +0100

    add idea

---
+++ app/build/generated/ap_generated_sources/debug/out/com/example/tmdb_project/Data/UserDao_Impl.java
@@ -0,0 +1,202 @@
+package com.example.tmdb_project.Data;
+
+import android.database.Cursor;
+import androidx.room.EntityInsertionAdapter;
+import androidx.room.RoomDatabase;
+import androidx.room.RoomSQLiteQuery;
+import androidx.room.util.CursorUtil;
+import androidx.room.util.DBUtil;
+import androidx.sqlite.db.SupportSQLiteStatement;
+import java.lang.Class;
+import java.lang.Integer;
+import java.lang.Long;
+import java.lang.Override;
+import java.lang.String;
+import java.lang.SuppressWarnings;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+@SuppressWarnings({"unchecked", "deprecation"})
+public final class UserDao_Impl implements UserDao {
+  private final RoomDatabase __db;
+
+  private final EntityInsertionAdapter<User> __insertionAdapterOfUser;
+
+  public UserDao_Impl(RoomDatabase __db) {
+    this.__db = __db;
+    this.__insertionAdapterOfUser = new EntityInsertionAdapter<User>(__db) {
+      @Override
+      public String createQuery() {
+        return "INSERT OR ABORT INTO `user` (`email`,`password`,`birthdate`) VALUES (?,?,?)";
+      }
+
+      @Override
+      public void bind(SupportSQLiteStatement stmt, User value) {
+        if (value.getEmail() == null) {
+          stmt.bindNull(1);
+        } else {
+          stmt.bindString(1, value.getEmail());
+        }
+        if (value.getPassword() == null) {
+          stmt.bindNull(2);
+        } else {
+          stmt.bindString(2, value.getPassword());
+        }
+        if (value.getBirthdate() == null) {
+          stmt.bindNull(3);
+        } else {
+          stmt.bindLong(3, value.getBirthdate());
+        }
+      }
+    };
+  }
+
+  @Override
+  public void insertUser(final User user) {
+    __db.assertNotSuspendingTransaction();
+    __db.beginTransaction();
+    try {
+      __insertionAdapterOfUser.insert(user);
+      __db.setTransactionSuccessful();
+    } finally {
+      __db.endTransaction();
+    }
+  }
+
+  @Override
+  public List<User> getAll() {
+    final String _sql = "SELECT * FROM user";
+    final RoomSQLiteQuery _statement = RoomSQLiteQuery.acquire(_sql, 0);
+    __db.assertNotSuspendingTransaction();
+    final Cursor _cursor = DBUtil.query(__db, _statement, false, null);
+    try {
+      final int _cursorIndexOfEmail = CursorUtil.getColumnIndexOrThrow(_cursor, "email");
+      final int _cursorIndexOfPassword = CursorUtil.getColumnIndexOrThrow(_cursor, "password");
+      final int _cursorIndexOfBirthdate = CursorUtil.getColumnIndexOrThrow(_cursor, "birthdate");
+      final List<User> _result = new ArrayList<User>(_cursor.getCount());
+      while(_cursor.moveToNext()) {
+        final User _item;
+        _item = new User();
+        final String _tmpEmail;
+        if (_cursor.isNull(_cursorIndexOfEmail)) {
+          _tmpEmail = null;
+        } else {
+          _tmpEmail = _cursor.getString(_cursorIndexOfEmail);
+        }
+        _item.setEmail(_tmpEmail);
+        final String _tmpPassword;
+        if (_cursor.isNull(_cursorIndexOfPassword)) {
+          _tmpPassword = null;
+        } else {
+          _tmpPassword = _cursor.getString(_cursorIndexOfPassword);
+        }
+        _item.setPassword(_tmpPassword);
+        final Long _tmpBirthdate;
+        if (_cursor.isNull(_cursorIndexOfBirthdate)) {
+          _tmpBirthdate = null;
+        } else {
+          _tmpBirthdate = _cursor.getLong(_cursorIndexOfBirthdate);
+        }
+        _item.setBirthdate(_tmpBirthdate);
+        _result.add(_item);
+      }
+      return _result;
+    } finally {
+      _cursor.close();
+      _statement.release();
+    }
+  }
+
+  @Override
+  public User loadByEmail(final String userEmail) {
+    final String _sql = "SELECT * FROM user WHERE email = ?";
+    final RoomSQLiteQuery _statement = RoomSQLiteQuery.acquire(_sql, 1);
+    int _argIndex = 1;
+    if (userEmail == null) {
+      _statement.bindNull(_argIndex);
+    } else {
+      _statement.bindString(_argIndex, userEmail);
+    }
+    __db.assertNotSuspendingTransaction();
+    final Cursor _cursor = DBUtil.query(__db, _statement, false, null);
+    try {
+      final int _cursorIndexOfEmail = CursorUtil.getColumnIndexOrThrow(_cursor, "email");
+      final int _cursorIndexOfPassword = CursorUtil.getColumnIndexOrThrow(_cursor, "password");
+      final int _cursorIndexOfBirthdate = CursorUtil.getColumnIndexOrThrow(_cursor, "birthdate");
+      final User _result;
+      if(_cursor.moveToFirst()) {
+        _result = new User();
+        final String _tmpEmail;
+        if (_cursor.isNull(_cursorIndexOfEmail)) {
+          _tmpEmail = null;
+        } else {
+          _tmpEmail = _cursor.getString(_cursorIndexOfEmail);
+        }
+        _result.setEmail(_tmpEmail);
+        final String _tmpPassword;
+        if (_cursor.isNull(_cursorIndexOfPassword)) {
+          _tmpPassword = null;
+        } else {
+          _tmpPassword = _cursor.getString(_cursorIndexOfPassword);
+        }
+        _result.setPassword(_tmpPassword);
+        final Long _tmpBirthdate;
+        if (_cursor.isNull(_cursorIndexOfBirthdate)) {
+          _tmpBirthdate = null;
+        } else {
+          _tmpBirthdate = _cursor.getLong(_cursorIndexOfBirthdate);
+        }
+        _result.setBirthdate(_tmpBirthdate);
+      } else {
+        _result = null;
+      }
+      return _result;
+    } finally {
+      _cursor.close();
+      _statement.release();
+    }
+  }
+
+  @Override
+  public Integer checkUser(final String email, final String password) {
+    final String _sql = "SELECT Count(*) FROM User WHERE email = ? AND password = ?";
+    final RoomSQLiteQuery _statement = RoomSQLiteQuery.acquire(_sql, 2);
+    int _argIndex = 1;
+    if (email == null) {
+      _statement.bindNull(_argIndex);
+    } else {
+      _statement.bindString(_argIndex, email);
+    }
+    _argIndex = 2;
+    if (password == null) {
+      _statement.bindNull(_argIndex);
+    } else {
+      _statement.bindString(_argIndex, password);
+    }
+    __db.assertNotSuspendingTransaction();
+    final Cursor _cursor = DBUtil.query(__db, _statement, false, null);
+    try {
+      final Integer _result;
+      if(_cursor.moveToFirst()) {
+        final Integer _tmp;
+        if (_cursor.isNull(0)) {
+          _tmp = null;
+        } else {
+          _tmp = _cursor.getInt(0);
+        }
+        _result = _tmp;
+      } else {
+        _result = null;
+      }
+      return _result;
+    } finally {
+      _cursor.close();
+      _statement.release();
+    }
+  }
+
+  public static List<Class<?>> getRequiredConverters() {
+    return Collections.emptyList();
+  }
+}
